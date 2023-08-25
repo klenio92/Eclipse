164df43fe72e0161835f832d89fa26f718c40a6a
User <User@DESKTOP-5EUGTCV>
2023-08-25 13:32:55 -0300

    Threads e tela grafica - mini projeto

---
+++ Primeiro_Projeto/src/cursojava/thread/ExecutandoTela.java
@@ -0,0 +1,9 @@
+package cursojava.thread;
+
+public class ExecutandoTela {
+
+	public static void main(String[] args) {
+		
+		TimeTela timeTela = new TimeTela();
+	}	
+}
--- Primeiro_Projeto/src/cursojava/thread/thread.java
+++ Primeiro_Projeto/src/cursojava/thread/thread.java
@@ -1,39 +1,49 @@
 package cursojava.thread;
 
+import javax.swing.JOptionPane;
+
 public class thread {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		new Thread() {
-			public void run() {
-				for (int pos = 0; pos < 10; pos++) {
-					System.out.println("Executando rotina!");
+		Thread threadRunOne = new Thread(thread1);
+		threadRunOne.start();
 
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			};
-		}.start();
-		/*--------------------------------------------------------------*/
-		new Thread() {
-			public void run() {
-				for (int pos = 0; pos < 10; pos++) {
-					System.out.println("Executando rotina 2!!");
+		Thread threadRunTwo = new Thread(thread2);
+		threadRunTwo.start();
 
-					try {
-						Thread.sleep(2000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			};
-		}.start();
-
+		JOptionPane.showMessageDialog(null, "Sistema rodando");
 	}
 
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 10; pos++) {
+				System.out.println("Executando rotina!");
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 10; pos++) {
+				System.out.println("Executando rotina 2!!");
+
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
 }
